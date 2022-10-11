1da746b96970298d5c64e723ea51a9f6ea67d133
Deemero Saidi <neroalex93@gmail.com>
2022-10-11 13:48:17 +0800

    remove package dart:html

--- crise_apps/lib/screen/home_page copy.dart
+++ crise_apps/lib/screen/home_page copy.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: unnecessary_const
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
