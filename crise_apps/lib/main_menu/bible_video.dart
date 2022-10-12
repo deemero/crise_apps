d2ddff9e08386c9259a0e723fad78ee92a51190d
Deemero Saidi <neroalex93@gmail.com>
2022-10-12 18:45:57 +0800

    adding url https link data

--- crise_apps/lib/main_menu/bible_video.dart
+++ crise_apps/lib/main_menu/bible_video.dart
@@ -1,7 +1,10 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BibleVideo extends StatefulWidget {
   const BibleVideo({super.key});
@@ -11,6 +14,8 @@ class BibleVideo extends StatefulWidget {
 }
 
 class _BibleVideoState extends State<BibleVideo> {
+  // String? get url => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +31,36 @@ class _BibleVideoState extends State<BibleVideo> {
         children: [
           Card(
             color: Colors.grey,
-            child: ListTile(
-              title: Text("List Item 1"),
+            child: ElevatedButton(
+              child: Text("Kejadian"),
+              onPressed: () async {
+                String url = "https://www.youtube.com/watch?v=piOWD-z95x4&t=0s";
+                await launch(url);
+                // if (await canLaunch(url)) {
+
+                //   await launch(
+                //     url,
+                //     // forceSafariVC: true,
+                //     forceWebView: false,
+
+                //     // enableJavaScript: true,
+                //     // enableDomStorage: true,
+                //     // webOnlyWindowName: '_self',
+                //   );
+                // } else {
+                //   print("invalid");
+                // }
+
+                // final Uri launchUri = Uri(
+                //   scheme: 'tel',
+                //   path: "+601129530841",
+                // );
+                // if (await canLaunch(launchUri.toString())) {
+                //   await launch(launchUri.toString());
+                // } else {
+                //   print("object");
+                // }
+              },
             ),
           )
         ],
